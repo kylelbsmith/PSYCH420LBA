3f25f99e59bc1ef86e7988c8ae663ef471698943
enyuez <anniezhang139@gmail.com>
2022-12-22 14:50:59 -0500

    plot display fix

--- lba.rkt
+++ lba.rkt
@@ -7,7 +7,7 @@
 (define n 2) ; number of choices (n ≥ 0) 
 (define bs '(15 10)) ; decision threshold - set one if common across all; else, list individual thresholds in same order as in means
 (define A 5) ; max range of starting k (0 ≤ k ≤ A)
-(define means '(10 1)) ; average means for each choice (length of means should be equal to n)
+(define means '(10 15)) ; average means for each choice (length of means should be equal to n)
 (define s 0.1) ; standard deviation of means, s ≥ 0
 
 ;; check user input
@@ -20,6 +20,8 @@
 
 ; check if s >= 0
 
+; n ≥ 2
+
 
 
 ;; Now that all variables have been checked, we are ready to run! We use the buildlba functions to build our accumulators:
@@ -28,4 +30,6 @@
 ;; visualize accumulators
 
 ; for each set of accumulator data, create plot using (plot-a driftrate threshold start):
-  
+(for ([acc accs])
+  (display (plot-a (second acc) (third acc) (first acc))))
+
